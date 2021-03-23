5a72a1bdaa6782d210f572bfd5deaf7db41ad2f4
khanhduy0112 <nguyenkhanhduy21123@gmail.com>
2021-03-23 23:42:15 +0700

    sdsd

--- src/App.js
+++ src/App.js
@@ -1,14 +1,61 @@
-import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom';
-import Main from './Main';
+import './App.css';
+import React, { useEffect, useState } from 'react'
+import CustomAppBar from './CustomAppBar';
+import MainSection from './MainSection';
+import { makeStyles } from '@material-ui/core';
+import Sidebar from './Sidebar';
+import { fetchCoinStats } from './service/fetchData'
+import Footer from './Footer';
+
+const useStyles = makeStyles((theme) => ({
+  appContainer: {
+    background: '#fff'
+  },
+  root: {
+    display: 'flex',
+    minHeight: '100vh',
+    padding: theme.spacing(2)
+  },
+  mainSection: {
+    width: '100%',
+    background: 'red'
+  },
+  sideBar: {
+    width: '30%',
+    background: 'black'
+  }
+
+}))
 
 function App() {
+  const classes = useStyles();
+  const [coins, setCoins] = useState({})
+  const [coinNames, setCoinNames] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [language, setLanguage] = useState("US");
 
+  useEffect(() => {
+    setIsLoading(true)
+    fetchCoinStats().then(
+      data => {
+        setCoinNames(Object.getOwnPropertyNames(data.data));
+        setCoins(data.data)
+        setIsLoading(false)
+      }
+    )
+
+  }, [])
   return (
-    <BrowserRouter basename={window.location.pathname || ''}>
-      <Route exact path="/" component={Main} />
-    </BrowserRouter>
-  );
+    <div className={classes.appContainer}>
+      <CustomAppBar setLanguage={setLanguage} />
+      <main className={classes.root}>
+        <MainSection language={language} coins={coins} coinNames={coinNames} isLoading={isLoading} className={classes.mainSection} />
+        <Sidebar coins={coins} coinNames={coinNames} isLoading={isLoading} className={classes.mainSection} className={classes.sideBar} />
+      </main>
+      <Footer />
+    </div >
+
+  )
 }
 
 export default App;
