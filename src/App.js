0a15ac7b05eac7417726d8fc8120383e68b6335c
khanhduy0112 <nguyenkhanhduy21123@gmail.com>
2021-03-23 23:36:48 +0700

    fix blank page when deploy

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom';
 import Main from './Main';
 
 function App() {
 
   return (
-    <Main />
+    <BrowserRouter basename={window.location.pathname || ''}>
+      <Route exact path="/" component={Main} />
+    </BrowserRouter>
   );
 }
 
